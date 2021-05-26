b9ed8cbd958f0c8cfe0bc987a86746dd3ea62382
Edchel Stephen <edchelstephens@gmail.com>
2021-05-27 07:59:49 +0800

    str and repr methods.

--- polls/models/abstract.py
+++ polls/models/abstract.py
@@ -6,3 +6,6 @@ class NamedModel(models.Model):
 
     class Meta:
         abstract = True
+
+    def __str__(self):
+        return self.name
--- polls/models/question.py
+++ polls/models/question.py
@@ -6,6 +6,9 @@ class Question(NamedModel):
     """The poll question model."""
     date_published = models.DateTimeField("date published")
 
+    def __repr__(self):
+        return f"Question(name={self.name})"
+
 class Choice(NamedModel):
     """Poll question answer choices model with votes."""
     question = models.ForeignKey(
@@ -14,3 +17,9 @@ class Choice(NamedModel):
         related_name="choices"
     )
     votes = models.IntegerField(default=0)
+
+    def __repr__(self):
+        return f"Choice(name={self.name}, question={repr(self.question)}, votes={self.votes}"
+    
+    def __str__(self):
+        return self.name
