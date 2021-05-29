01790ea4c8ae93670d61432bf1b6371d14835810
Edchel Stephen <edchelstephens@gmail.com>
2021-05-29 08:24:28 +0800

    remove unused response import.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse, response
+from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hi! Welcome to ed's pollsite! This is the site index.")
