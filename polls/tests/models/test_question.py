61ed1a2f3e3c3176bd6837989866534ac55271f8
Edchel Stephen <edchelstephens@gmail.com>
2021-06-04 08:00:00 +0800

    test str and repr methods for question.

--- polls/tests/models/test_question.py
+++ polls/tests/models/test_question.py
@@ -8,12 +8,21 @@ from polls.models import Question
 class QuestionModelTests(TestCase):
     """Test case for Question Model."""
 
+    def test_str(self):
+        """test Question str method."""
+        name = "test"
+        question = Question(name="test")
+        
+        self.assertEqual(str(question), name)
+        self.assertEqual(question.__str__(), name)
+
     def test_repr(self):
         """test Question repr method."""
         name = "test"
         question = Question.objects.create(name=name, date_published=timezone.now())
 
         self.assertEqual(repr(question), "Question(id={}, name={})".format(question.id, name))
+        self.assertEqual(question.__repr__(), "Question(id={}, name={})".format(question.id, name))
 
     def test_was_published_recently_with_future_question(self):
         """was_published_recently() returns False for future questions."""
