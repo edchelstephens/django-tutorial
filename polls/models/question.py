58acbf612fe1034ff69ab60d2e61505195d94af1
Edchel Stephen <edchelstephens@gmail.com>
2021-06-04 08:16:09 +0800

    remove Choice from question.py and str method which was already inhertied from NamedModel.

--- polls/models/question.py
+++ polls/models/question.py
@@ -9,9 +9,6 @@ class Question(NamedModel):
     """The poll question model."""
     date_published = models.DateTimeField("date published")
 
-    def __str__(self):
-        return self.name
-
     def __repr__(self):
         return "Question(id={}, name={})".format(
             self.id,
@@ -23,20 +20,3 @@ class Question(NamedModel):
         now = timezone.now()
         recent = now - datetime.timedelta(days=1)
         return recent <= self.date_published <= now
-
-class Choice(NamedModel):
-    """Poll question answer choices model with votes."""
-    question = models.ForeignKey(
-        Question, 
-        on_delete=models.CASCADE, 
-        related_name="choices"
-    )
-    votes = models.IntegerField(default=0)
-
-    def __repr__(self):
-        return "Choice(id={}, name={}, question={}, votes={})".format(
-            self.id,
-            self.name,
-            repr(self.question),
-            self.votes
-        )
