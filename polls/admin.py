83a01318ec679574fbc5b3ec0e37a5b90e9f7282
Edchel Stephen <edchelstephens@gmail.com>
2021-06-15 21:42:05 +0800

    explore admin.ModelAdmin class with Question, adding QuestionAdmin class to edit the order of the form fields on admin.

--- polls/admin.py
+++ polls/admin.py
@@ -3,5 +3,10 @@ from django.contrib import admin
 from polls.models.question import Question
 from polls.models.choice import Choice
 
-admin.site.register(Question)
 admin.site.register(Choice)
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fields = ["date_published", "name"]
+
+admin.site.register(Question, QuestionAdmin)
