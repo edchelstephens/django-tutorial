6623224cac82cbcfaed21d2b98c980280412fd01
Edchel Stephen <edchelstephens@gmail.com>
2021-06-16 08:18:58 +0800

    was_published_recently admin fields settings.

--- polls/models/question.py
+++ polls/models/question.py
@@ -20,3 +20,8 @@ class Question(NamedModel):
         now = timezone.now()
         recent = now - datetime.timedelta(days=1)
         return recent <= self.date_published <= now
+    
+    was_published_recently.admin_order_field = "date_published"
+    was_published_recently.boolean = True
+    was_published_recently.short_description = "Published recently?"
+    
