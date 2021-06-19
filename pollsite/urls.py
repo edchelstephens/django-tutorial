cc750cc528276ae2cf4cde2ee3401fcca0623a41
Edchel Stephen <edchelstephens@gmail.com>
2021-06-19 15:32:48 +0800

    Make polls as index.

--- pollsite/urls.py
+++ pollsite/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from polls.views import IndexView
 
 urlpatterns = [
+    path("", IndexView.as_view()),
     path("admin/", admin.site.urls),
     path("polls/", include("polls.urls")),
 ]
