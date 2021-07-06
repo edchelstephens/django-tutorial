16bb3dd5259c1704c3fc882be643d6c11002683e
Edchel Stephen <edchelstephens@gmail.com>
2021-07-07 07:22:48 +0800

    usage of on_delete=models.SET().

--- songs/models.py
+++ songs/models.py
@@ -15,7 +15,8 @@ class Artist(models.Model):
         return self.name
 
 def get_unkown_artist():
-    return Artist.objects.get_or_create(name="unkown")
+    unknown_artist, created =  Artist.objects.get_or_create(name="unkown")
+    return unknown_artist
 
 class Album(models.Model):
 
@@ -27,8 +28,19 @@ class Album(models.Model):
 class Song(models.Model):
 
     artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-    album = models.ForeignKey(Album, on_delete=models.RESTRICT)    
+    album = models.ForeignKey(Album, on_delete=models.RESTRICT) 
+    feat = models.ForeignKey(
+        Artist, 
+        on_delete=models.SET(get_unkown_artist), 
+        related_name="featured_artist", 
+        null=True, 
+        blank=True
+    )   
 
     def __repr__(self) -> str:
-        return "Song(artist={}, album={})".format(repr(self.artist), repr(self.album))
+        return "Song(artist={}, album={}, feat={})".format(
+            repr(self.artist), 
+            repr(self.album), 
+            repr(self.feat)
+    )
 
