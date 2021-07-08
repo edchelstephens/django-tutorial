ddf920cc96155d0a3255dd5157e7962f0a9f977a
Edchel Stephen <edchelstephens@gmail.com>
2021-07-09 07:44:36 +0800

    musician, group, membership to illustrate many to many relationship with through intermediary model.

--- songs/models.py
+++ songs/models.py
@@ -44,3 +44,68 @@ class Song(models.Model):
             repr(self.feat)
     )
 
+
+class Musician(models.Model):
+
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __repr__(self) -> str:
+        return "Musician(name={}, groups={})".format(
+            self.name,
+            self.get_groups()
+    )
+
+    def get_groups(self):
+        return [ g.name for g in self.groups.all() ]
+
+class Group(models.Model):
+
+    name = models.CharField(max_length=50)
+    members = models.ManyToManyField(
+        Musician, 
+        through="Membership",
+        related_name="groups"
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+    def __repr__(self) -> str:
+        return "Group(name={}, members={})".format(
+            self.name,
+            self.get_members()
+        )
+
+    def get_members(self):
+        return [ m.name for m in self.members.all() ]
+
+
+class Membership(models.Model):
+
+    musician = models.ForeignKey(
+        Musician,
+        on_delete=models.CASCADE,
+        related_name="memberships"
+    )
+
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        related_name="group_memberships"
+    )
+
+    date_joined = models.DateTimeField(null=True, blank=True)
+    invite_reason = models.CharField(max_length=120)
+
+
+    def __str__(self) -> str:
+        return "{} - {}".format(self.group, self.musician)
+
+    def __repr__(self) -> str:
+        return "Membership(musician={}, group={})".format(
+            self.musician,
+            self.group
+        )
